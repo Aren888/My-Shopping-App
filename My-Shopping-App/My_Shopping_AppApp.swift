c27806760966a8cb779a1f541e560e3af102986c
Aren888 <aren.musayelyan555@gmail.com>
2024-01-22 01:29:23 +0400

    Initial Commit

---
+++ My-Shopping-App/My_Shopping_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Shopping_AppApp.swift
+//  My-Shopping-App
+//
+//  Created by Solicy Ios on 22.01.24.
+//
+
+import SwiftUI
+
+@main
+struct My_Shopping_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
